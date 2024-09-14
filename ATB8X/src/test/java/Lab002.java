2029078bd639b007624b3271d10e04b4f9f39ac3
Shiv027 <shivankumar1627@gmail.com>
2024-09-14 21:50:13 +0530

    JAVA

--- ATB8X/src/test/java/Lab002.java
+++ ATB8X/src/test/java/Lab002.java
@@ -1,2 +1,13 @@
 public class Lab002 {
+
+    public static void main(String[] args) {
+
+        double a =10;
+        double b = 20;
+
+        System.out.println( a+b );
+        System.out.println( a-b );
+        System.out.println( a/b );
+        System.out.println( a*b );
+    }
 }
