e781194fd2ad3a9a81f6001c6b03d13927598aa2
Shiv027 <shivankumar1627@gmail.com>
2024-09-18 20:15:28 +0530

    JAVA

---
+++ ATB8X/src/test/java/Lab007.java
@@ -0,0 +1,9 @@
+public class Lab007 {
+    public static void main(String[] args) {
+int course = 100;
+float gst =18.45f;
+
+int total_price = course+ (int)gst;
+        System.out.println(total_price);
+
+    }}
---
+++ ATB8X/src/test/java/Lab008.java
@@ -0,0 +1,15 @@
+public class Lab008 {
+    public static void main(String[] args) {
+ String s1= "hello";
+ String s2 ="java";
+ int a =10;
+ int b=20;
+
+      //
+        // System.out.println(s1+s2+(a+b));
+       // System.out.println(a+b+s1+s2);
+        System.out.println(a+s1+s2);
+
+    //    string + int = concatination happens
+    }
+}
